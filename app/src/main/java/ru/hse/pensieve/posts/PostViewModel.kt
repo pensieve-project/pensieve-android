456b83a850587bb3cd78886d2fa23fe927c07dc9
kolesovasonya <soffakolesova@gmail.com>
2025-03-03 20:40:00 +0300

    fixes: rename thread to theme

--- app/src/main/java/ru/hse/pensieve/posts/PostViewModel.kt
+++ app/src/main/java/ru/hse/pensieve/posts/PostViewModel.kt
@@ -1,25 +1,19 @@
 package ru.hse.pensieve.posts
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
-import ru.hse.pensieve.posts.models.PostRequest
 import ru.hse.pensieve.posts.repository.PostRepository
 import java.util.UUID
 
 class PostViewModel: ViewModel() {
     private val postRepository = PostRepository()
 
-    fun createPost(text: String, threadId: UUID) {
+    fun createPost(text: String, themeId: UUID) {
         viewModelScope.launch {
             try {
-                postRepository.createPost(text, threadId);
+                postRepository.createPost(text, themeId);
             } catch (e: Exception) {
-                println("Exception!! In createPost PostViewModel")
-                println(e.cause)
-                println(e.stackTrace)
                 println(e.message)
             }
         }
