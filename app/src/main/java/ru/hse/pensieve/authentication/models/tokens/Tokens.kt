fe98a99464088b11f27275834095097518fd6472
kolesovasonya <soffakolesova@gmail.com>
2025-03-05 17:26:24 +0300

    fixes: fix deserialization

--- app/src/main/java/ru/hse/pensieve/authentication/models/tokens/Tokens.kt
+++ app/src/main/java/ru/hse/pensieve/authentication/models/tokens/Tokens.kt
@@ -1,9 +1,6 @@
 package ru.hse.pensieve.authentication.models.tokens
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class Tokens(
-    @JsonProperty("accessToken") val accessToken: String? = null,
-    @JsonProperty("refreshToken") val refreshToken: String? = null
-) {
-}
+    val accessToken: String? = null,
+    val refreshToken: String? = null
+)
