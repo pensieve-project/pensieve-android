bfc6caa8de88eec2dceaaa5b75e2ea7e1a4a486f
kolesovasonya <soffakolesova@gmail.com>
2025-04-30 21:59:36 +0300

    fixes: fix profile model

--- app/src/main/java/ru/hse/pensieve/profiles/models/Profile.kt
+++ app/src/main/java/ru/hse/pensieve/profiles/models/Profile.kt
@@ -1,11 +1,30 @@
 package ru.hse.pensieve.profiles.models
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.UUID
 
 data class Profile(
+    @JsonProperty("authorId")
     val authorId: UUID? = null,
+
+    @JsonProperty("avatar")
     val avatar: ByteArray? = null,
+
+    @JsonProperty("description")
     val description: String? = null,
-    val likedThemesIds: MutableList<UUID> = mutableListOf(),
-    val likedPostsIds: MutableList<UUID> = mutableListOf()
+
+    @JsonProperty("likedThemesIds")
+    val likedThemesIds: MutableList<UUID>? = null,
+
+    @JsonProperty("likedPostsIds")
+    val likedPostsIds: MutableList<UUID>? = null,
+
+    @JsonProperty("subscriptionsCount")
+    val subscriptionsCount: Int? = null,
+
+    @JsonProperty("subscribersCount")
+    val subscribersCount: Int? = null,
+
+    @JsonProperty("isVip")
+    val isVip: Boolean? = null
 )
