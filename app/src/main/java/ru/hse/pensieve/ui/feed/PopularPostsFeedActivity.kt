9b1f89294a3ce17f314065109e3a8ca0610cd925
Golovachev Sergey <golovacev905@gmail.com>
2025-06-23 19:36:08 +0300

    feat: add posts search

--- app/src/main/java/ru/hse/pensieve/ui/feed/PopularPostsFeedActivity.kt
+++ app/src/main/java/ru/hse/pensieve/ui/feed/PopularPostsFeedActivity.kt
@@ -3,11 +3,13 @@ package ru.hse.pensieve.ui.feed
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.activity.viewModels
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.LiveData
 import ru.hse.pensieve.R
 import ru.hse.pensieve.databinding.ActivityFeedBinding
 import ru.hse.pensieve.feed.FeedViewModel
 import ru.hse.pensieve.posts.models.Post
+import ru.hse.pensieve.search.SearchViewModel
 import ru.hse.pensieve.ui.ToolbarActivity
 import ru.hse.pensieve.ui.posts_view.PostsDataSource
 import ru.hse.pensieve.ui.posts_view.PostsFeedFragment
@@ -24,23 +26,96 @@ class PopularDataSource(
     }
 }
 
+class SearchDataSource(
+    private val searchViewModel: SearchViewModel
+) : PostsDataSource {
+    override fun getPosts(): LiveData<List<Post>> {
+        return searchViewModel.posts
+    }
+
+    override fun loadMorePosts() {
+        searchViewModel.searchPosts()
+    }
+}
+
 
 class PopularPostsFeedActivity :  ToolbarActivity() {
     private lateinit var binding: ActivityFeedBinding
+    private val searchViewModel: SearchViewModel by viewModels()
     private val feedViewModel: FeedViewModel by viewModels()
     private lateinit var popularFragment: PostsFeedFragment
+    private lateinit var postsSearchFragment: PostsFeedFragment
+    private var isSearchActive = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFeedBinding.inflate(layoutInflater)
         setContentView(binding.root)
         popularFragment = PostsFeedFragment.newInstance(PopularDataSource(feedViewModel))
+        postsSearchFragment = PostsFeedFragment.newInstance(SearchDataSource(searchViewModel))
 
         supportFragmentManager.beginTransaction()
+            .add(R.id.feed_container, postsSearchFragment)
+            .hide(postsSearchFragment)
             .add(R.id.feed_container, popularFragment)
             .commit()
 
         setupToolBar()
+        setupSearchView()
+        showPostsFeed()
+    }
+
+    private fun setupSearchView() {
+        val searchView = binding.searchView
+
+        searchView.setOnQueryTextFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                isSearchActive = true
+                showSearchResults()
+            }
+        }
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let { searchViewModel.searchPosts(it) }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText.isNullOrEmpty()) {
+                    closeSearch()
+                    return true
+                }
+                if (!isSearchActive) {
+                    isSearchActive = true
+                    showSearchResults()
+                }
+                searchViewModel.searchPosts(newText)
+                return true
+            }
+        })
+
+        searchView.setOnCloseListener {
+            if (isSearchActive) {
+                closeSearch()
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun showSearchResults() {
+        supportFragmentManager.beginTransaction()
+            .hide(popularFragment)
+            .show(postsSearchFragment)
+            .commit()
+    }
+
+    private fun closeSearch() {
+        isSearchActive = false
+        binding.searchView.clearFocus()
+        searchViewModel.clearPostsSearch()
         showPostsFeed()
     }
 
